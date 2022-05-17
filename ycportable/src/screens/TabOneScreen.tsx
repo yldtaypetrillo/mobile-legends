3cd478029543fc8786baac01623ce44b7383ae23
Rorymacauley <37596940+Rorymacauley@users.noreply.github.com>
2022-05-17 15:05:08 +0100

    Initial campaign fetch with hardcoded URL and auth (#3)
    * inital campaign fetch with hardcoded URL and auth

--- ycportable/src/screens/TabOneScreen.tsx
+++ ycportable/src/screens/TabOneScreen.tsx
@@ -1,13 +1,36 @@
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 import { RootTabScreenProps } from '../../types';
 
-export default function TabOneScreen({
-  navigation,
-}: RootTabScreenProps<'TabOne'>) {
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [state, setState] = useState<any[]>([])
+
+  useEffect(() => {
+    fetch(
+      "https://viserion.yieldify-dev.com/v2/organizations/1/websites/1/campaigns",
+      {headers: new Headers({
+        // TODO: populate from auth state (currently requires a manual enter)
+        'Authorization': '',
+        'Content-Type': 'application/json'
+    }), }
+    )
+    .then(response => response.json())
+    .then(json => {
+      setState(json); // access json.body here
+    }).catch((err) =>{
+      alert(err);
+    })
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      {state.map((campaign) => {
+        return (
+        <View>
+          <Text>{`name: ${campaign.name}`}</Text>
+        </View>);
+      })}
     </View>
   );
 }
@@ -21,5 +44,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
   },
 });
