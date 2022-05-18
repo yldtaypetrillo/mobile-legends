ec0b935bc20b52fa263840b478a4c4ab554bf97d
JordanWu <jordan.wu@yieldify.com>
2022-05-18 15:29:34 +0100

    delete tableOneScreen.tsx

--- ycportable/src/components/EmailCampaignImageComponent.tsx
+++ ycportable/src/components/EmailCampaignImageComponent.tsx
@@ -3,6 +3,6 @@ import { Image } from 'react-native';
 import { EmailCampaign } from '../..';
 export function EmailCampaignImageComponent({ campaign }: returnImageProps) {
     return (
-        <Image source={{ uri: `${(campaign as EmailCampaign).specs.emailConfiguration[0].previewUrl}` }} style={{ width: 30, height: 15 }} ></Image>
+        <Image source={{ uri: `${(campaign as EmailCampaign).specs.emailConfiguration[0].previewUrl}` }} style={{ width: 305, height: 159 }} ></Image>
     )
 }
